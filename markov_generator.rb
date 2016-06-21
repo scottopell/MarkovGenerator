fa30fb6aa9b2f6d2df056577283eda7fe55bbd3d
Scott Opell <me@scottopell.com>
2016-06-21 15:45:07 -0400

    implements inverse transform sampling for more accurate sampling

--- markov_generator.rb
+++ markov_generator.rb
@@ -1,27 +1,26 @@
 # USAGE:
-# ./markov_generator file1 file2 file3 seed_word
-
+# ./markov_generator file1 [file2 file3] [seed_word]
 require 'pry'
-PRECISION = 10000
-ROUND_VALUE = Math.log(PRECISION, 10).round
 
 class Word
-  attr_accessor :count
-  def initialize
+  attr_accessor :count, :string
+  def initialize string
     @count = 0
+    @string = string
   end
 
   def prob total_count
     @count.to_f / total_count
   end
 end
-class Entry
+
+class MarkovEntry
   attr_accessor :total_words
   def initialize preceding_word
     @words = Hash.new
     @preceding_word = preceding_word
     @total_words = 0
-    @determiner = Array.new PRECISION, nil
+    @cumulative = Array.new
   end
 
   def add_word word
@@ -30,37 +29,29 @@ class Entry
     @total_words += 1
 
     if @words[word].nil?
-      @words[word] = Word.new
+      @words[word] = Word.new word
     end
     @words[word].count += 1
   end
 
-  def recalculate_determiner
-    current_end = 0
-    #puts "Preceding Word: #{@preceding_word}"
-    @words.each_pair do |key, value|
-      new_end = current_end + (value.prob(@total_words).round(ROUND_VALUE) * PRECISION).round
-      #puts "\t#{key}\t prob\t#{value.prob(@total_words).round(ROUND_VALUE)}\t [#{current_end}, #{new_end}]"
-      @determiner.fill(key, current_end..new_end)
-      current_end = new_end
-    end
-    if current_end != PRECISION
-      #puts current_end
+  def calculate_probabilities
+    # inverse transform sampling
+    frequencies = @words.values.collect{|w| w.count}
+    @cumulative = Array.new frequencies.length, 0
+    @cumulative[0] = frequencies[0]
+    frequencies.each_with_index do |f, ind|
+      @cumulative[ind] = @cumulative[ind - 1] + f if ind > 0
     end
   end
 
-  def get_value
-    # pick from the current words according to their probabilities
-    # so if you have 2 words, A with 2 occurrences and B with 4, then
-    # A has a 1/3 chance and B has a 2/3 chance of being picked
-    #
-    return @determiner[rand(PRECISION)]
+  def sample
+    # inverse transform sampling
+    random_index = @cumulative.bsearch_index{ |n| n >= rand(@cumulative.last) }
+    return @words.keys[random_index]
   end
 end
 
 freq = Hash.new
-# 0..-2 gives from the first element to the second to last element
-# [0,1,2,3,4][0..-2] => [0,1,2,3]
 ARGV.each do |f|
   if !f.end_with?(".txt")
     next
@@ -70,7 +61,7 @@ ARGV.each do |f|
   words.each_cons(2) do |word_tuple|
     key = word_tuple.first.downcase
     if freq[key].nil?
-      freq[key] = Entry.new key
+      freq[key] = MarkovEntry.new key
     end
     freq[key].add_word word_tuple.last.downcase
   end
@@ -78,7 +69,7 @@ end
 
 total_words = 0
 freq.each_pair do |key, value|
-  value.recalculate_determiner
+  value.calculate_probabilities
   total_words += value.total_words
 end
 
@@ -92,13 +83,10 @@ end
 puts "Trump on #{current_seed}:"
 print "\"#{current_seed} "
 loop do
-  current = freq[current_seed].get_value
+  current = freq[current_seed].sample
   print current
   current_seed = current
-  if current.nil?
-    puts ""
-    puts "Current Seed: #{current_seed}"
-  end
+
   if current.chars.last == '.'
     break
   else
